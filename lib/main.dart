2fd806a3ed97f1a2866cb59fa989ee4e6a5d3d2a
Prerna <prerna3573.be20@chitkaraedu.in>
2022-12-18 00:46:59 +0530

    User Data state Management

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_flutter/providers/user_provider.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/responsive_layout.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_flutter/screens/login_screen.dart';
 import 'package:instagram_flutter/screens/signup_screen.dart';
 import 'package:instagram_flutter/utils/colors.dart';
+import 'package:provider/provider.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   if(kIsWeb){
@@ -29,37 +31,44 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Instagram Clone',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: mobileBackgroundColor
+    return MultiProvider(
+       providers: [
+        ChangeNotifierProvider(
+          create: (_)=>UserProvider()
+        ),
+       ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Instagram Clone',
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: mobileBackgroundColor
+        ),
+       // home:
+        home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+             if(snapshot.connectionState==ConnectionState.active){
+              if(snapshot.hasData){
+                return const ResponsiveLayout(
+                   webScreenLayout:WebScreenLayout() ,
+                   mobileScreenLayout: MobileScreenLayout(),
+                 );
+              }
+              else if(snapshot.hasError){
+                return Center(
+                  child: Text('${snapshot.error}'),
+                );
+              }
+             }
+             if(snapshot.connectionState==ConnectionState.waiting){
+               return const Center(
+                child: CircularProgressIndicator(
+                  color: primaryColor,
+                  ),);
+             }
+             return LoginScreen();
+        },)
       ),
-     // home:
-      home: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-           if(snapshot.connectionState==ConnectionState.active){
-            if(snapshot.hasData){
-              return const ResponsiveLayout(
-                 webScreenLayout:WebScreenLayout() ,
-                 mobileScreenLayout: MobileScreenLayout(),
-               );
-            }
-            else if(snapshot.hasError){
-              return Center(
-                child: Text('${snapshot.error}'),
-              );
-            }
-           }
-           if(snapshot.connectionState==ConnectionState.waiting){
-             return const Center(
-              child: CircularProgressIndicator(
-                color: primaryColor,
-                ),);
-           }
-           return LoginScreen();
-      },)
     );
   }
 }
--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_flutter/models/user.dart' as model;
+import 'package:provider/provider.dart';
+
+import '../providers/user_provider.dart';
 class MobileScreenLayout extends StatelessWidget {
   const MobileScreenLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-     return Scaffold(body: Center(child: Text('This is mobile'),),);
+    model.User user=Provider.of<UserProvider>(context).getUser;
+     return Scaffold(body: Center(child: Text(user.bio),),);
   }
 }
