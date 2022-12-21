7c2098199a3ff164be6fabf14b188591e9197163
Prerna <prerna3573.be20@chitkaraedu.in>
2022-12-22 01:52:09 +0530

    Built posts screen and fetched it's data from firebase

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/screens/add_post_screen.dart';
+import 'package:instagram_flutter/screens/feed_screen.dart';
 const webScreenSize=600;
 const homeScreenItems=[
-   Text("Home"),
+        FeedScreen(),
         Text("Search"),
         AddPostScreen(),
         Text("notification"),
